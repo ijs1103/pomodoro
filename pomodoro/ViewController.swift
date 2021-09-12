639fe216be933dd6e1548010850a97f49ab7bdef
Gunter <xc200925@gmail.com>
2021-09-12 15:36:37 +0900

    타이머 상태에 따라 화면 구성이 달라지게 구현

--- pomodoro/ViewController.swift
+++ pomodoro/ViewController.swift
@@ -7,6 +7,12 @@
 
 import UIKit
 
+enum TimerStatus {
+  case start
+  case pause
+  case end
+}
+
 class ViewController: UIViewController {
 
   @IBOutlet weak var timerLabel: UILabel!
@@ -17,17 +23,55 @@ class ViewController: UIViewController {
   @IBOutlet weak var toggleButton: UIButton!
 
   var duration = 60
+  var timerStatus: TimerStatus = .end
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    self.configureToggleButton()
   }
 
   @IBAction func tapCancelButton(_ sender: UIButton) {
+    switch self.timerStatus {
+    case .start, .pause:
+      self.timerStatus = .end
+      self.cancelButton.isEnabled = false
+      self.setTimerInfoViewVisible(isHidden: true)
+      self.datePicker.isHidden = false
+      self.toggleButton.isSelected = false
+
+    default:
+      break
+    }
   }
 
   @IBAction func tapToggleButton(_ sender: UIButton) {
     self.duration = Int(self.datePicker.countDownDuration)
-    debugPrint("\(self.duration)")
+    switch self.timerStatus {
+    case .end:
+      self.timerStatus = .start
+      self.setTimerInfoViewVisible(isHidden: false)
+      self.datePicker.isHidden = true
+      self.toggleButton.isSelected = true
+      self.cancelButton.isEnabled = true
+
+    case .start:
+      self.timerStatus = .pause
+      self.toggleButton.isSelected = false
+
+    case .pause:
+      self.timerStatus = .start
+      self.toggleButton.isSelected = true
+    }
+  }
+
+  func configureToggleButton() {
+    self.toggleButton.setTitle("시작", for: .normal)
+    self.toggleButton.setTitle("일시정지", for: .selected)
+  }
+
+  func setTimerInfoViewVisible(isHidden: Bool) {
+    self.timerLabel.isHidden = isHidden
+    self.progressView.isHidden = isHidden
   }
 }
 
