2f2c23f15d0b4955709330a3278ca80fffc49d8a
Gunter <xc200925@gmail.com>
2021-09-12 13:46:41 +0900

    아울렛 변수 액션 함수 정의

--- pomodoro/ViewController.swift
+++ pomodoro/ViewController.swift
@@ -8,9 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+
+  @IBOutlet weak var timerLabel: UILabel!
+  @IBOutlet weak var progressView: UIProgressView!
+  @IBOutlet weak var datePicker: UIDatePicker!
+
+  @IBOutlet weak var cancelButton: UIButton!
+  @IBOutlet weak var toggleButton: UIButton!
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+  }
+
+  @IBAction func tapCancelButton(_ sender: UIButton) {
+  }
+
+  @IBAction func tapToggleButton(_ sender: UIButton) {
   }
 }
 
